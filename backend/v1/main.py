06391ba54c3995606fa952172572e210a1706bc9
Tiago Nora <33982771+TiagoNora@users.noreply.github.com>
2024-04-09 03:20:29 +0100

    feat: Helping the poor by allowing them to run the code, only with the command python main.py

--- backend/v1/main.py
+++ backend/v1/main.py
@@ -1,9 +1,12 @@
+import uvicorn
 from fastapi import FastAPI
 from routes import checkRouter, userRouter
 from models import *
-
 
 app = FastAPI()
 
 app.include_router(checkRouter.router)
 app.include_router(userRouter.router)
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", port=5000, log_level="info")
