bf8c83339f3ce42d6bd933d39a7ea85341da8f7b
Tiago Nora <33982771+TiagoNora@users.noreply.github.com>
2024-04-12 01:15:16 +0100

    feat: User can add the genres that he/she likes

--- backend/v1/models/user.py
+++ backend/v1/models/user.py
@@ -1,17 +1,20 @@
-from sqlmodel import Field, SQLModel
-from typing import Optional
+from sqlmodel import Field, SQLModel, JSON
+from typing import Optional, List
 from sqlalchemy import Column, String
+
 
 class User(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     name: str
     email: str
     password: str
+    genres: List[str] = Field(sa_column=Column(JSON))
 
 class UserCreate(SQLModel):
     name: str
     email: str
     password: str
+    genres: List[str]
 
 
 class UserUpdate(SQLModel):
@@ -22,3 +25,10 @@ class UserUpdate(SQLModel):
 class UserLogin(SQLModel): 
     email: str 
     password: str
+
+class UserRead(SQLModel):
+    id: int
+    name: str 
+    email: str  
+    password: str
+    genres: List[str]
--- backend/v1/routes/userRouter.py
+++ backend/v1/routes/userRouter.py
@@ -8,21 +8,21 @@ from sqlmodel import select
 
 router = APIRouter(prefix='/users', tags=['Users'])
 
-@router.post("/", summary="Create a user")
-async def createUser(*, session: AsyncSession = Depends(get_db), user: UserCreate):
-    query = select(User).where(User.email == user.email)
+@router.post("/", response_model=UserRead, summary="Create a user")
+async def createUser(*, session: AsyncSession = Depends(get_db), userCreate: UserCreate):
+    query = select(User).where(User.email == userCreate.email)
     user = await session.execute(query)
     if user.scalars().first():
         raise HTTPException(status_code=400, detail="User already exists with this email")
-    db_user = User(name=user.name, email=user.email, password=user.password)
+    db_user = User(name=userCreate.name, email=userCreate.email, password=userCreate.password, genres=userCreate.genres)
     session.add(db_user)
     await session.commit()
     await session.refresh(db_user)
     return db_user
 
 @router.post("/login", summary="Login")
-async def loginUser(*, session: AsyncSession = Depends(get_db), user: UserLogin):
-    query = select(User).where(User.email == user.email).where(User.password == user.password)
+async def loginUser(*, session: AsyncSession = Depends(get_db), userLogin: UserLogin):
+    query = select(User).where(User.email == userLogin.email).where(User.password == userLogin.password)
     result = await session.execute(query)
     user = result.scalars().first()
     if not user:
@@ -30,14 +30,14 @@ async def loginUser(*, session: AsyncSession = Depends(get_db), user: UserLogin)
     else:
         return {"status": "Verified"}
 
-@router.get("/{user_id}", summary="Get user by id")
+@router.get("/{user_id}", response_model=UserRead, summary="Get user by id")
 async def getUserByID(*, session: AsyncSession = Depends(get_db), user_id: int):
     user = await session.get(User, user_id)
     if not user:
         raise HTTPException(status_code=404, detail="user not found")
     return user
 
-@router.patch("/{user_id}", summary="Update a user")
+@router.patch("/{user_id}", response_model=UserRead, summary="Update a user")
 async def update_user(*, session: AsyncSession = Depends(get_db), user_id: int, user: UserUpdate):
     db_user = await session.get(User, user_id)
     if not db_user:
