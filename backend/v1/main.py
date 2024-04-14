5cf2e9a9a78f25024adc67e5da90a68c22bf8907
Tiago Nora <33982771+TiagoNora@users.noreply.github.com>
2024-04-14 03:09:37 +0100

    feat: Endpoint for retrieving data for non personalized recommendations

--- backend/v1/main.py
+++ backend/v1/main.py
@@ -1,7 +1,7 @@
 import uvicorn
 from database import *
 from fastapi import FastAPI
-from routes import checkRouter, userRouter,genresRouter
+from routes import checkRouter, userRouter,genresRouter, recommendationRouter
 
 from models import *
 
@@ -10,6 +10,7 @@ app = FastAPI()
 app.include_router(checkRouter.router)
 app.include_router(userRouter.router)
 app.include_router(genresRouter.router)
+app.include_router(recommendationRouter.router)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", port=5000, log_level="info")
