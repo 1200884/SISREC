f43a1349e5aa84fee9d06779258439879dba7153
Tiago Nora <33982771+TiagoNora@users.noreply.github.com>
2024-04-16 17:17:55 +0100

    feat: Updated movies' router

--- backend/v1/routes/moviesRouter.py
+++ backend/v1/routes/moviesRouter.py
@@ -9,13 +9,23 @@ from fastapi.responses import JSONResponse
 import csv
 import os
 import pandas as pd
+from random import randint
 
 router = APIRouter(prefix='/movies', tags=['Movies'])
 
 @router.get("/search", summary="Search movie")
-async def getMovie(*, q:str):
+async def getMovie(*, title:str, year:int):
     script_dir = os.path.dirname(__file__)
     df_movies = pd.read_csv(os.path.join(script_dir, '../recommender/dataset/movies.csv'))
-    result = df_movies[df_movies['title'].str.contains(q)]
-    json = result.head(5).reset_index().to_dict(orient='records')
+    result = df_movies[df_movies['title'].str.contains(title) & (df_movies['year'] == year)]
+    json = result.reset_index().to_dict(orient='records')
+    return JSONResponse(content=json)
+
+@router.get("/randomMovie", summary="Returns random movie")
+async def getRandomMovie():
+    script_dir = os.path.dirname(__file__)
+    df_movies = pd.read_csv(os.path.join(script_dir, '../recommender/dataset/movies.csv'))
+    number = randint(0, df_movies.shape[0])
+    result = df_movies[df_movies.index == number]
+    json = result.reset_index().to_dict(orient='records')
     return JSONResponse(content=json)
