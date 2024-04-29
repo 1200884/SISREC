92f8acbeb9f73a16f47744b0e62886d140d1cd6e
Tiago Nora <33982771+TiagoNora@users.noreply.github.com>
2024-04-29 17:00:30 +0100

    fix: Fixing the bug when searching for a movie by title

--- backend/v1/routes/moviesRouter.py
+++ backend/v1/routes/moviesRouter.py
@@ -14,18 +14,18 @@ from random import randint
 router = APIRouter(prefix='/movies', tags=['Movies'])
 
 @router.get("/search/{title}", summary="Search movie")
-async def getMovie(*, title:str):
+async def getMovie(title: str):
     titleAlt = title.lower()
     script_dir = os.path.dirname(__file__)
-    df_movies = pd.read_csv(os.path.join(script_dir, '../recommender/dataset/movies.csv'))
-    result = df_movies[df_movies['title'].str.contains(titleAlt)]
-    json = result.reset_index().to_dict(orient='records')
+    df_movies = pd.read_csv(os.path.join(script_dir, '../utils/movies_full.csv'))
+    result = df_movies[df_movies['titleLower'].str.contains(titleAlt)]
+    json = result.head(5).reset_index().to_dict(orient='records')
     return JSONResponse(content=json)
 
 @router.get("/randomMovie", summary="Returns random movie")
 async def getRandomMovie():
     script_dir = os.path.dirname(__file__)
-    df_movies = pd.read_csv(os.path.join(script_dir, '../recommender/dataset/movies.csv'))
+    df_movies = pd.read_csv(os.path.join(script_dir, '../utils/movies_full.csv'))
     number = randint(0, df_movies.shape[0])
     result = df_movies[df_movies.index == number]
     json = result.reset_index().to_dict(orient='records')
--- backend/v1/utils/addTitle.py
+++ backend/v1/utils/addTitle.py
@@ -16,9 +16,11 @@ file_path = os.path.join(script_dir,'./movies_with_imdb.csv')
 
 file_path1 = os.path.join(script_dir,'./movies_full.csv')
 
-merged_df = pd.read_csv(file_path)
+merged_df = pd.read_csv(file_path1)
 
+merged_df = merged_df.fillna('')
+merged_df.to_csv(file_path1, index=False)
 
-for ind in merged_df.index:
-    merged_df.at[ind,'titleLower'] = merged_df.at[ind,'title'].lower()
-    merged_df.to_csv(file_path1, index=False)
+#for ind in merged_df.index:
+#    merged_df.at[ind,'titleLower'] = merged_df.at[ind,'title'].lower()
+#    merged_df.to_csv(file_path1, index=False)
