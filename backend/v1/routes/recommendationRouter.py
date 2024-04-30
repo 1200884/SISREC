08b40acdb44ff01eea73beb8f17f0718fa79f6ea
Tiago Nora <33982771+TiagoNora@users.noreply.github.com>
2024-04-30 21:42:30 +0100

    refactor: Changing data source from local files to database

--- backend/v1/routes/recommendationRouter.py
+++ backend/v1/routes/recommendationRouter.py
@@ -3,35 +3,46 @@ from fastapi.responses import JSONResponse
 import pandas as pd
 import os
 import json
+from sqlalchemy import create_engine
+from dotenv import load_dotenv
+load_dotenv()
 
 router = APIRouter(prefix='/recommendation', tags=['Recomendation'])
+engine = create_engine(os.getenv('DATABASE_URL_FILE'))
 
 @router.get("/nonpersonalized", summary="Get non-personalized recommendations")
 async def nonPersonalised():
     script_dir = os.path.dirname(__file__)
     path = os.path.join(script_dir, '../utils/NonPersonalized.json')
     if os.stat(path).st_size == 0:
-        df_rating = pd.read_csv(os.path.join(script_dir, '../recommender/dataset/ratings.csv'))
-        df_movie = pd.read_csv(os.path.join(script_dir,'../utils/movies_full.csv'))
+        sql_query = """
+            SELECT 
+                movieid, 
+                title, 
+                genres,
+                year,
+                url,
+                COUNT(stars) AS count, 
+                AVG(stars) AS mean,
+                (
+                    (COUNT(stars) / (COUNT(stars) + 1000)) * AVG(stars) +
+                    (1000 / (COUNT(stars) + 1000)) * AVG(stars)
+                ) AS weighted_rating
+            FROM 
+                rating a
+            JOIN 
+                movie b
+            ON 
+                a.movie_id = b.movieid
+            GROUP BY 
+                movieid, title, genres, year, url
+            ORDER BY 
+                weighted_rating DESC
+        """
 
-        df_rating = df_rating[['movieId', 'rating']]
-        #df_movie = df_movie[['movieId', 'title']]
-
-        grouped_df = (
-            pd.merge(df_rating, df_movie, on='movieId')
-            .groupby(['movieId', 'title', 'genres', 'imdbId', 'year', 'url','titleLower'])
-            .agg(count=('rating', 'count'), mean=('rating', 'mean'))
-        )
-
-        m = 1000
-        grouped_df['weighted_rating'] = (
-            (grouped_df['count'] / (grouped_df['count'] + m)) * grouped_df['mean'] +
-            (m / (grouped_df['count'] + m)) * grouped_df['mean'].mean()
-        )
-
-        sorted_df = grouped_df.sort_values(by='weighted_rating', ascending=False)
+        grouped_df = pd.read_sql_query(sql_query, con=engine)
     
-        json_result = sorted_df.head(5).reset_index().to_dict(orient='records')
+        json_result = grouped_df.head(5).reset_index().to_dict(orient='records')
         with open(path, "w") as file:
             json.dump(json_result, file)
     
@@ -48,28 +59,34 @@ def nonPersonalizedToFile():
     try:
         f = open(path, "x")
         with open(path, "w") as file:
-            script_dir = os.path.dirname(__file__)
-            df_rating = pd.read_csv(os.path.join(script_dir, '../recommender/dataset/ratings.csv'))
-            df_movie = pd.read_csv(os.path.join(script_dir,'../utils/movies_full.csv'))
+            sql_query = """
+                SELECT 
+                    movieid, 
+                    title, 
+                    genres, 
+                    year,
+                    url,
+                    COUNT(stars) AS count, 
+                    AVG(stars) AS mean,
+                    (
+                        (COUNT(stars) / (COUNT(stars) + 1000)) * AVG(stars) +
+                        (1000 / (COUNT(stars) + 1000)) * AVG(stars)
+                    ) AS weighted_rating
+                FROM 
+                    rating a
+                JOIN 
+                    movie b
+                ON 
+                    a.movie_id = b.movieid
+                GROUP BY 
+                    movieid, title, genres, year, url
+                ORDER BY 
+                    weighted_rating DESC
+            """
 
-            df_rating = df_rating[['movieId', 'rating']]
-            #df_movie = df_movie[['movieId', 'title']]
-
-            grouped_df = (
-            pd.merge(df_rating, df_movie, on='movieId')
-            .groupby(['movieId', 'title', 'genres', 'imdbId', 'year', 'url','titleLower'])
-            .agg(count=('rating', 'count'), mean=('rating', 'mean'))
-            )
-
-            m = 1000
-            grouped_df['weighted_rating'] = (
-            (grouped_df['count'] / (grouped_df['count'] + m)) * grouped_df['mean'] +
-            (m / (grouped_df['count'] + m)) * grouped_df['mean'].mean()
-            )
-
-            sorted_df = grouped_df.sort_values(by='weighted_rating', ascending=False)
+            grouped_df = pd.read_sql_query(sql_query, con=engine)
     
-            json_result = sorted_df.head(5).reset_index().to_dict(orient='records')
+            json_result = grouped_df.head(5).reset_index().to_dict(orient='records')
             json.dump(json_result, file)
     except:
         print("File already exists")
