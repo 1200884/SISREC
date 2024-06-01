2027ab25a4723f7d25332074af8ddae69aa30d69
Tiago Nora <33982771+TiagoNora@users.noreply.github.com>
2024-06-01 23:50:16 +0100

    refactor: "Improved" the similar movies endpoint

--- backend/v1/routes/moviesRouter.py
+++ backend/v1/routes/moviesRouter.py
@@ -30,36 +30,33 @@ async def getRandomMovie(*, session: AsyncSession = Depends(get_db)):
     movie = await session.get(Movie, number)
     return movie
 
+script_dir = os.path.dirname(__file__)
+tags_df = pd.read_csv(os.path.join(script_dir, "../utils/tags.csv"))
+movies_df = pd.read_csv(os.path.join(script_dir, "../utils/movies_full.csv"))
+tags_df.set_index('movieId', inplace=True)
+movies_df.set_index('movieId', inplace=True)
+tags_df['tag'] = tags_df['tag'].str.lower()
+tags_df['tag'] = tags_df['tag'].astype(str)
+movie_tags = tags_df.groupby('movieId')['tag'].apply(lambda x: ' '.join(x)).reset_index()
+movies = pd.merge(movies_df, movie_tags, on='movieId', how='left')
+movies['tag'] = movies['tag'].fillna('')
+
+tfidf = TfidfVectorizer(stop_words='english')
+tfidf_matrix = tfidf.fit_transform(movies['tag'])
+
+cosine_sim = cosine_similarity(tfidf_matrix, tfidf_matrix)
+
 @router.get("/similarMovies/{title}", summary="Returns 5 similar movies")
 async def getSimilarMovies(*, title: str):
-    script_dir = os.path.dirname(__file__)
-    tags_df = pd.read_csv(os.path.join(script_dir, "../utils/tags.csv"))
-    movies_df = pd.read_csv(os.path.join(script_dir, "../utils/movies_full.csv"))
-    tags_df['tag'] = tags_df['tag'].str.lower()
+    matching_rows = movies[movies['title'].str.contains(title, case=False, na=False)]
+    if len(matching_rows) == 0:
+        return []
+    idx = matching_rows.index[0]
     
-    # Convert all tags to string to avoid TypeError
-    tags_df['tag'] = tags_df['tag'].astype(str)
-    
-    movie_tags = tags_df.groupby('movieId')['tag'].apply(lambda x: ' '.join(x)).reset_index()
-    movies = pd.merge(movies_df, movie_tags, on='movieId', how='left')
-    movies['tag'] = movies['tag'].fillna('')
-
-    tfidf = TfidfVectorizer(stop_words='english')
-    tfidf_matrix = tfidf.fit_transform(movies['tag'])
-
-    indices = pd.DataFrame(movies['title'], index=movies.index).drop_duplicates()
-    matching_rows = indices[indices['title'].str.contains(title, case=False, na=False)]
-    idx = matching_rows.iloc[0].name
-    
-    # Compute the cosine similarity between the given movie and all others
-    cosine_sim = cosine_similarity(tfidf_matrix[idx], tfidf_matrix)
-    
-    # Get similarity scores and sort them
-    sim_scores = list(enumerate(cosine_sim[0]))
+    sim_scores = list(enumerate(cosine_sim[idx]))
     sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
-    sim_scores = sim_scores[1:6]  # Get the top 5 similar movies
+    sim_scores = sim_scores[1:6] 
     
-    # Get the movie indices of the top 5 similar movies
     movie_indices = [i[0] for i in sim_scores]
     
     # Return the top 5 similar movies
